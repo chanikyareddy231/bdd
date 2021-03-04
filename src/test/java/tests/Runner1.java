c5a4d200b055c09f43d781db09664eaf7786de00
chanikyareddy231 <79044191+chanikyareddy231@users.noreply.github.com>
2021-03-04 08:02:22 +0530

    Update Runner1.java

--- src/test/java/tests/Runner1.java
+++ src/test/java/tests/Runner1.java
@@ -3,7 +3,7 @@ package tests;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-//For Smoke Testing
+//For Smoke Testing in api testing
 @CucumberOptions(features= {"src\\test\\resources\\features"},
 				 glue= {"gluecode"},
 				 tags="@smoketest",
